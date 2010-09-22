60066aa91c1944de17edbd197730217b37a09a04
Ixtli <cg@325i.org>
2010-09-22 17:39:37 -0400

    Added comment about SDL.

--- source/tests/test.cc
+++ source/tests/test.cc
@@ -1,6 +1,8 @@
 #include "SDL.h"
 #include "SDL_main.h"
 
+// The SDL_main.h installed for OS X says that the following
+// needs to be present.  I wont argue.
 #ifdef __cplusplus
 extern "C"
 #endif
