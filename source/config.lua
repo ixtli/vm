ea89091b3f739236c966a59abb63227cba5ad4e3
Ixtli <cg@325i.org>
2010-11-04 21:14:06 -0400

    Added default config script and updated INSTALL

---
+++ source/config.lua
@@ -0,0 +1,15 @@
+-- YAAA Virtual Machine Configuration
+program = "out"
+memory_dump = "memory.dump"
+
+-- YAAA Machine Description
+
+-- Memory size in bytes
+memory_size = 1024
+
+
+-- Instruction timings
+
+-- Memory
+read_cycles = 100
+write_cycles = 100
