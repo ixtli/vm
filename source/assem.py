cc36a074559779a5f9c312d33f8db1a21a1fa767
John Rafferty <hukdunfonixwerks@gmail.com>
2010-11-08 23:00:38 -0500

    now labels arent removed just skipped from the asm

--- source/assem.py
+++ source/assem.py
@@ -139,12 +139,12 @@ class Assembler:
                 if previous_was_label == 0:
                     self.label[label[0]] = instruction_index;
                     previous_was_label = 1;
-                    self.infile.remove(line);
+                    #self.infile.remove(line);
                 else:
                     # If we don't do this, this function needs to be recurive
                     # and I don't see a reason to have multiple names
                     # for one line
-                    self.infile.remove(line);
+                    #self.infile.remove(line);
                     print "Only one label per location, please."
             else:
                 instruction_index += 1;     
@@ -166,119 +166,121 @@ class Assembler:
             
             line = self.explodeOp(lines)
             
-            i = 0;
-            immediate = 0;
-            if line[0] in self.condition_codes:
-                cond_code = self.condition_codes[line[0]];
-                instruction = line[1];          #the operation (add, etc..)a
-                # instead of creating an offset for all other calls of line[]
-                # just move everything back 1 position
-                for i in range(0, len(line)-1):
-                    line[i] = line[i+1];
-            else:
-                cond_code = self.condition_codes["al"]; # default condition code
-                instruction = line[0];
-            
-            dest = line[1];
-            
-            if len(line) < 3:               #destination reg
-                src1 = "00"
-            else:
-                src1 = line[2];            #first source register
-            
-            if len(line) > 3:
-                src2 = line[3];             #second source register, Op2
-            else:
-                src2 = "00"
-            
-            bin = cond_code;
-            
-            # check if its an arithmetic/logic/compare/test instruction
-            if (instruction in self.arithmetic_logic) or \
-                (instruction in self.comp_test): 
-                i += 1;
+            if len(line) > 1:
                 
-                # mov is a special case
-                if instruction == "mov":
-                    src2 = src1
-                    src1 = "0000"
-                
-                # check if there is an r (register), or if its immediate
-                if (src2[0] == "r"): 
-                    immediate = 0;
+                i = 0;
+                immediate = 0;
+                if line[0] in self.condition_codes:
+                    cond_code = self.condition_codes[line[0]];
+                    instruction = line[1];          #the operation (add, etc..)a
+                    # instead of creating an offset for all other calls of line[]
+                    # just move everything back 1 position
+                    for i in range(0, len(line)-1):
+                        line[i] = line[i+1];
                 else:
-                    immediate = 1;
+                    cond_code = self.condition_codes["al"]; # default condition code
+                    instruction = line[0];
                 
-                bindest = dest[1:];
+                dest = line[1];
                 
-                if src1[0] == 'r':
-                    binsrc1 = src1[1:];
+                if len(line) < 3:               #destination reg
+                    src1 = "00"
                 else:
-                    binsrc1 = src1;
+                    src1 = line[2];            #first source register
                 
-                if src2[0] == 'r':
-                    binsrc2 = src2[1:];
+                if len(line) > 3:
+                    src2 = line[3];             #second source register, Op2
                 else:
-                    binsrc2 = src2;
+                    src2 = "00"
                 
-                bin += "00";
-                # I flag
-                bin += str(immediate);
-                
-                if instruction in self.arithmetic_logic:
-                        # opcode
-                        bin += self.arithmetic_logic[instruction];
-                else:
-                        # opcode
-                        bin += self.comp_test[instruction];
-                
-                # s flag
-                bin += "1";
-                bin += self.decimal_to_binary(int(binsrc1)); # Rs
-                bin += self.decimal_to_binary(int(bindest)); # Rd
-                # padding if its not being shifted
-                bin += "00000";
-                
-                if immediate == 0:
-                    # Op2
-                    bin += self.decimal_to_binary(int(binsrc2));
-                else:
-                    bin += self.decimal_to_binary(int(src2));
-                
-                print bin;
-                outfile.write(bin + "\n");
-                
-            elif (instruction in self.branch):
-                branch_loc = dest;
-                # begin formatting the binary string
                 bin = cond_code;
-                if (instruction == "b"):
-                    bin += self.branch[instruction];
-                elif (instruction == "bl"):
-                    bin += self.branch[instruction];
                 
-                offset = abs(instruction_index - self.label[branch_loc]);
-                
-                # Are we going forward or back?
-                if instruction_index > self.label[branch_loc] :
-                    # if we are, add one because after the instruction is loaded
-                    # the pc is incremented, so it's going to actually be one
-                    # AHEAD of the instruction being executed 
-                    offset += 1;
-                
-                print "Offset: " + str(offset)
-                
-                # format the offset as 2's comp. and add it to the instruction
-                binoffset = self.decimal_to_binary(offset);
-                actual_offset = int(binoffset, 2)-(1<<24);
-                bin += self.decimal_to_binary(abs(actual_offset));
-                
-                print bin;
-                outfile.write(bin + "\n");
-                
-            else:
-                print "wrong instruction";
-                instruction_index += 1;
+                # check if its an arithmetic/logic/compare/test instruction
+                if (instruction in self.arithmetic_logic) or \
+                    (instruction in self.comp_test): 
+                    i += 1;
+                    
+                    # mov is a special case
+                    if instruction == "mov":
+                        src2 = src1
+                        src1 = "0000"
+                    
+                    # check if there is an r (register), or if its immediate
+                    if (src2[0] == "r"): 
+                        immediate = 0;
+                    else:
+                        immediate = 1;
+                    
+                    bindest = dest[1:];
+                    
+                    if src1[0] == 'r':
+                        binsrc1 = src1[1:];
+                    else:
+                        binsrc1 = src1;
+                    
+                    if src2[0] == 'r':
+                        binsrc2 = src2[1:];
+                    else:
+                        binsrc2 = src2;
+                    
+                    bin += "00";
+                    # I flag
+                    bin += str(immediate);
+                    
+                    if instruction in self.arithmetic_logic:
+                            # opcode
+                            bin += self.arithmetic_logic[instruction];
+                    else:
+                            # opcode
+                            bin += self.comp_test[instruction];
+                    
+                    # s flag
+                    bin += "1";
+                    bin += self.decimal_to_binary(int(binsrc1)); # Rs
+                    bin += self.decimal_to_binary(int(bindest)); # Rd
+                    # padding if its not being shifted
+                    bin += "00000";
+                    
+                    if immediate == 0:
+                        # Op2
+                        bin += self.decimal_to_binary(int(binsrc2));
+                    else:
+                        bin += self.decimal_to_binary(int(src2));
+                    
+                    print bin;
+                    outfile.write(bin + "\n");
+                    
+                elif (instruction in self.branch):
+                    branch_loc = dest;
+                    # begin formatting the binary string
+                    bin = cond_code;
+                    if (instruction == "b"):
+                        bin += self.branch[instruction];
+                    elif (instruction == "bl"):
+                        bin += self.branch[instruction];
+                    
+                    offset = abs(instruction_index - self.label[branch_loc]);
+                    
+                    # Are we going forward or back?
+                    if instruction_index > self.label[branch_loc] :
+                        # if we are, add one because after the instruction is loaded
+                        # the pc is incremented, so it's going to actually be one
+                        # AHEAD of the instruction being executed 
+                        offset += 1;
+                    
+                    print "Offset: " + str(offset)
+                    
+                    # format the offset as 2's comp. and add it to the instruction
+                    binoffset = self.decimal_to_binary(offset);
+                    actual_offset = int(binoffset, 2)-(1<<24);
+                    bin += self.decimal_to_binary(abs(actual_offset));
+                    
+                    print bin;
+                    outfile.write(bin + "\n");
+                    
+                else:
+                    print "wrong instruction";
+                    instruction_index += 1;
 
 if __name__ == "__main__":
     a = Assembler()
